037f3e7121d6dd3476b0f6ede9ed1cdfa1420ea8
mac <mac@macs-MacBook-Pro.local>
2024-05-08 12:29:41 +0100

    third commit

--- function.js
+++ function.js
@@ -1,3 +1,10 @@
+// ================= DEFAULT DISABLE ====================
+document.addEventListener('contextmenu', function (event) {
+  event.preventDefault();
+});
+
+
+// =================== IMAGE CARD =========================
 document.addEventListener("DOMContentLoaded", function() {
     const cardContainer = document.getElementById("cardContainer");
     const imageUrls = [
@@ -15,4 +22,42 @@ document.addEventListener("DOMContentLoaded", function() {
       cardContainer.appendChild(card);
     });
   });
+
+
+
+// ======================= SLIDER N CLOSE ===================================
+  const slideElement = document.getElementById("slide-in");
+  const closeElement = document.getElementById("close");
+  
+  closeElement.addEventListener("click", () => {
+    slideElement.style.display = "none";
+  });
+  
+  function showSlideElement() {
+    slideElement.style.display = "block";
+    slideElement.style.opacity = "1";
+    slideElement.style.transition = "opacity 0.5s ease-in-out";
+    setTimeout(() => {
+        slideElement.style.transition = "opacity 0.5s";
+        slideElement.style.opacity = "0";
+    }, 5000);
+  }
+  
+  function repeatSlideEffect() {
+      let count = 0;
+      let maxCount = 3;
+      let interval = 3000; // 3 seconds
+  
+      function repeat() {
+          if (count < maxCount) {
+              showSlideElement();
+            count++;
+              setTimeout(repeat, interval + 5000); // 5 seconds + 3 seconds interval
+          }
+      }
+      repeat();
+  }
+  
+  setTimeout(repeatSlideEffect, 0); // Run on page load
+  
   
